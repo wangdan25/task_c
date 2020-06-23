e1612c87900db93e2563d849a2a7d86766a71b0d
wangdan25 <1095134217@qq.com>
2020-06-23 22:57:28 +0800

    修改main函数实现过程

--- main.c
+++ main.c
@@ -8,18 +8,23 @@
 int main()
 {
     int step, start;
+    step = 2;
+    start =1;
     int len = 0;
-    int *n = &len;
-    char** data = reader("person.txt", n);
-    Person* person = create_josephus_person(data, n);
-    printf("约瑟夫环里原始的人是：\n");
-    show_people(person, n);
-    printf("**********************************************\n");
-    printf("请输入步进和起始值（用空格隔开）：\n");
-    scanf("%d %d",&step, &start);
-    Person* result = out_people(person, n, step, start);
+    josephus* self = josephus_new();
+    char** data = reader_data_new();
+    reader("person.txt", data, &len);
+    josephus_init(self,data, start, step, &len);
+    // printf("约瑟夫环里原始的人是：\n");
+    // person_show(self->people, &len);
+    // printf("**********************************************\n");
+    Person** result = malloc(100*sizeof(Person*));
+    josephus_get_result(self, result, &len);
     printf("淘汰的人是：\n");
-    show_people(result, n);
+    person_show(result, &len);
+    reader_data_destroy(data);
+    free(result);
+    josephus_destroy(self);
     system("pause");
     return 0;
 }
