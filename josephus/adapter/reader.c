5c6a970a5f07ff229a73af39a4fee22e5cbdc403
wangdan25 <1095134217@qq.com>
2020-06-23 22:52:34 +0800

    修改函数名

--- josephus/adapter/reader.c
+++ josephus/adapter/reader.c
@@ -4,57 +4,68 @@
 
 #include "reader.h"
 
-#define N 500
+#define N 100
+#define SUCCESS 1
+#define FALSE 0
+#define OPEN_FILE_FAILED -1;
 
-char** reader(char* file_path, int* n)
+char** reader_data_new()
+{  
+  char** data = malloc(N*sizeof(char**));  
+  return data;
+}
+
+void reader_data_destroy(char** data)
+{
+  free(data);
+}
+
+int reader(char* file_path,char** data, int* n)
 {
   FILE* fp = fopen(file_path, "r");
   if (fp == NULL)
   {
-    printf("Fail to open file! ");
-    return NULL;
+    return OPEN_FILE_FAILED;
   }
-  file_line(fp, n);
-  //为指针数组开辟空间
-  char** data = (char **)malloc(*n*sizeof(char*));
-  get_file_data(fp, n, data);
+  reader_get_file_line(fp, n);
+  reader_get_file_data(fp, n, data);
   fclose(fp);
-  return data;
+  return SUCCESS;
 }
 
-void file_line(FILE* file, int* n)
+int reader_get_file_line(FILE* file, int* n)
 {
   char temp[N] = {0};
   if (file == NULL)
   {
-    return;
+    return FALSE;
   }
   while(fgets(temp,N,file) != NULL)
   {
     (*n)++;
   }
   rewind(file);
+  return SUCCESS;
 }
 
-void get_file_data(FILE* file, int* line, char** data)
+int reader_get_file_data(FILE* file, int* line, char** data)
 {
   char temp[N] = {0};
   int i = 0;
   if (file == NULL)
   {
-    return;
+    return FALSE;
   }
   if (*line == -1)
   {
-    return;
+    return FALSE;
   }
   while(fgets(temp, N, file) != NULL)
   {
-		//给当前行分配内存
 		char* line_data = (char*)malloc(sizeof(char) * N);
-		//将行数据拷贝到空间中
 		strcpy(line_data, temp);
 		data[i++] = line_data;
-        memset(temp, 0, N);
+    memset(temp, 0, N);
   }
+  return SUCCESS;
 }
--- josephus/adapter/reader.h
+++ josephus/adapter/reader.h
@@ -2,10 +2,10 @@
 #ifndef READER_H
 #define READER_H
 
-char** reader(char* file_path, int* n);
-void file_line(FILE* file, int* n);
-void get_file_data(FILE* file, int* line, char** data);
-
-
+char** reader_data_new();
+void reader_data_destroy(char** data);
+int reader(char* file_path, char** data, int* n);
+int reader_get_file_line(FILE* file, int* n);
+int reader_get_file_data(FILE* file, int* line, char** data);
 
 #endif
